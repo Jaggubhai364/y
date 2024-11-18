d121674226aba33b8eb661aec7f99ee8e1bbca01
bjkumar03 <bjkumar180@gmail.com>
2024-11-18 15:52:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const mongoDB = require('./db');
 
 
 app.use((req,res,next)=>{
-      res.setHeader('Access-Control-Allow-Origin','https://z-ks8c-baijayanta-dalais-projects.vercel.app/
-');
+      res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
       res.header('Access-Control-Allow-Headers',
         'Origin,X-Requested-With,Content-Type,Accept'
       );
