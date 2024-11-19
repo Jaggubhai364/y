e962d5d587f2eeddc4b82c1b2acefb0c453dd5ac
bjkumar03 <bjkumar180@gmail.com>
2024-11-19 22:22:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoDB = require('./db');
 
 
 app.use((req,res,next)=>{
-      res.setHeader('Access-Control-Allow-Origin','https://z-7252.vercel.app/');
+      res.setHeader('Access-Control-Allow-Origin','https://z-vfo3.vercel.app/');
       res.header('Access-Control-Allow-Headers',
         'Origin,X-Requested-With,Content-Type,Accept'
       );
